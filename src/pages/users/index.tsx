33ef04ba17fcc0c98fd601e28bc0b82c6522363b
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-06-04 15:01:48 -0300

    Creating pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,6 +3,7 @@ import { Box,Button, Flex, Heading, Icon, Table, Thead, Tr, Th, Checkbox, Tbody,
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
  
 import { Header } from '../../components/Header'
+import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar'
 
 export default function UserList() {
@@ -71,6 +72,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
