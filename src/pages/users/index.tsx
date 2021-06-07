4ca79a6af8e6bf4264baa29663841d4c3ba97bd2
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-06-07 11:36:49 -0300

    Componentizing Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,23 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  Text,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
-import { Box,Button, Flex, Heading, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from '@chakra-ui/react';
-import { RiAddLine, RiPencilLine } from 'react-icons/ri'
- 
-import { Header } from '../../components/Header'
-import { Pagination } from '../../components/Pagination';
-import { Sidebar } from '../../components/Sidebar'
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
   return (
@@ -14,29 +27,28 @@ export default function UserList() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg='gray.800' p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-              <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
 
-              <Button
-                as="a"
-                size="sm"
-                fontSize="sm"
-                colorScheme="pink"
-                leftIcon={<Icon
-                as={RiAddLine}
-                fontSize="20"
-                />}
-              >
-                Criar novo
-              </Button>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
           </Flex>
-        
+
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink'/>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
                 <Th>Data de cadastro</Th>
@@ -45,28 +57,27 @@ export default function UserList() {
             </Thead>
             <Tbody>
               <Tr>
-                <Td px='6'>
-                  <Checkbox colorScheme='pink' />
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
-                    <Text fontWeight='bold'>Jorge Almeida</Text>
-                    <Text fontSize='sm' color='gray.300'>jorgebrunoalmeidadossantos@gmail.com</Text>
+                    <Text fontWeight="bold">Jorge Almeida</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      jorgebrunoalmeidadossantos@gmail.com
+                    </Text>
                   </Box>
                 </Td>
                 <Td>04 de Abril, 2021</Td>
                 <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon
-                      as={RiPencilLine} 
-                      fontSize="16"
-                    />}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                   >
-                  Editar
+                    Editar
                   </Button>
                 </Td>
               </Tr>
@@ -77,5 +88,5 @@ export default function UserList() {
         </Box>
       </Flex>
     </Box>
-  )
+  );
 }
